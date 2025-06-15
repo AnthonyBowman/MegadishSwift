8f4dded68a6f04bbdcd8a43ef493489ec2f95089
AnthonyBowman <brian@sherr.co.uk>
2025-06-15 10:20:05 +0100

    Working with now standard byte order

---
+++ MegadishUI/SettingsView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsView.swift
+//  MegadishUI
+//
+//  Created by Brian Bowman on 02/06/2025.
+//
+
+import Foundation
---
+++ MegadishUI/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewModel.swift
+//  MegadishUI
+//
+//  Created by Brian Bowman on 02/06/2025.
+//
+
+import Foundation
